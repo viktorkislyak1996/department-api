e6ee9db87d58d086fa373882ada43c5607faeddf
Viktor Kislyak <viktorkislyak1996@gmail.com>
2023-06-12 13:42:39 +0300

    customize admin panel

--- department/admin.py
+++ department/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 
 from department.models import Department, Employee
 
-admin.site.register(Department)
-admin.site.register(Employee)
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "created")
+    search_fields = ("title",)
+    list_filter = ("created",)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "middle_name",
+        "department",
+        "image_preview",
+        "position",
+        "salary",
+        "age",
+        "created",
+    )
+    search_fields = ("first_name", "last_name", "position")
+    list_filter = ("age", "created", "position", "department")
+
+
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Employee, EmployeeAdmin)
