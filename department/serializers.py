8ecfa8da41bccf23a6d25f474cee1debac2760c9
Viktor Kislyak <viktorkislyak1996@gmail.com>
2023-06-14 13:16:33 +0300

    add type hints

--- department/serializers.py
+++ department/serializers.py
@@ -9,7 +9,7 @@ class NullToEmptyModelSerializer(serializers.ModelSerializer):
     Replace all null values to empty string in serialized result.
     """
 
-    def to_representation(self, instance):
+    def to_representation(self, instance) -> dict:
         result = super().to_representation(instance)
         for i, v in result.items():
             if v is None:
@@ -36,10 +36,10 @@ class DepartmentSerializer(NullToEmptyModelSerializer):
     employee_count = serializers.SerializerMethodField()
     total_salary = serializers.SerializerMethodField()
 
-    def get_employee_count(self, instance):
+    def get_employee_count(self, instance: Department) -> int:
         return Employee.objects.filter(department=instance).count()
 
-    def get_total_salary(self, instance):
+    def get_total_salary(self, instance: Department) -> int:
         total = Employee.objects.filter(department=instance).aggregate(
             total=Sum("salary")
         )
